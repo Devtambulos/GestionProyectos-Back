98137796ee75d3e6de23457470bdec4db046601d
mreyesq <marcereyesq@gmail.com>
2021-11-09 22:36:57 -0500

    Instalar libreria typescript

--- db/db.ts
+++ db/db.ts
@@ -1,8 +1,9 @@
 import { connect } from 'mongoose';
 
 const connectDB = async () => {
-  return await connect(
-    'mongodb+srv://admin:admin@testmongoose.k0loc.mongodb.net/mongoose-test?retryWrites=true&w=majority'
+  return await connect(   
+    'mongodb+srv://MarcelaR:nreyes123@gestionproyectos.9onm9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    
   );
 };
 
