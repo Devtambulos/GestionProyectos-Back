19887b84a38856fbddc7e22995a467a081b4498e
jfelipeo <jfelipe_o@hotmail.com>
2021-11-12 17:30:37 -0500

    Instalacion de graphql, cors y dotenv y el nuevo index.ts

---
+++ graphql/types.ts
@@ -0,0 +1,12 @@
+import { gql } from 'apollo-server-express';
+
+const typeDefs = gql`
+  type Usuario {
+    nombre: String!
+  }
+  type Query {
+    Usuarios: [Usuario]
+  }
+`;
+
+export { typeDefs };
