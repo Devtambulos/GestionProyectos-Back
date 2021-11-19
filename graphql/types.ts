6f1b517dd0080ab78992e90a5f77111a3195a854
joalpe13 <joalpe3@gmail.com>
2021-11-19 14:21:12 -0500

    probando conexion

--- graphql/types.ts
+++ graphql/types.ts
@@ -4,9 +4,10 @@ import { tiposEnums } from '../models/enums/tipos';
 import { tiposInscripcion } from '../models/inscripcion/tipos';
 import { tiposAvance } from '../models/avance/tipos';
 import { tiposObjetivo } from '../models/objetivo/tipos';
+import { tiposProyecto } from '../models/proyecto/tipos';
 
 const tiposGlobales = gql`
 scalar Date
 `;
 
-export const tipos = [tiposGlobales,/*tiposObjetivo*/ , /* tiposInscripcion */,/*tiposAvance*/ ,tiposUsuario,tiposEnums];
+export const tipos = [tiposGlobales,/*tiposProyecto */ ,/*tiposObjetivo*/ , /* tiposInscripcion */,/*tiposAvance*/ ,tiposUsuario,tiposEnums];
