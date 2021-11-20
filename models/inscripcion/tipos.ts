e60879174b8e15cbf099161f1b55f197323c2a6c
JuanPabloRP <jpabloroldanp@gmail.com>
2021-11-20 12:13:55 -0500

    crud inscripcion listo

--- models/inscripcion/tipos.ts
+++ models/inscripcion/tipos.ts
@@ -3,7 +3,7 @@ import { gql } from "apollo-server-express";
 const tiposInscripcion = gql`
   type Inscripcion {
     _id: ID!
-    estado: Enum_EstadoInscripcion!
+    estado: Enum_EstadoInscripcion
     fechaIngreso: Date
     fechaEgreso: Date
     proyecto: Proyecto!
@@ -17,7 +17,7 @@ const tiposInscripcion = gql`
 
   type Mutation {
     crearInscripcion(
-      estado: Enum_EstadoInscripcion!
+      estado: Enum_EstadoInscripcion
       proyecto: String!
       estudiante: String!
     ): Inscripcion
