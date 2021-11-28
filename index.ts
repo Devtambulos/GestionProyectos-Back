5492f0ee0a5e82328117a28266e572286067be4f
jfelipeo <jfelipe_o@hotmail.com>
2021-11-28 11:42:31 -0500

    Rutas y componentes privados de acuerdo a rol

--- index.ts
+++ index.ts
@@ -5,12 +5,32 @@ import dotenv from 'dotenv';
 import conectarBD from './db/db';
 import { tipos } from './graphql/types';
 import { resolvers } from './graphql/resolvers';
+import { validateToken} from './utils/tokenUtils'
 
 dotenv.config();
+
+const getUserData = (token) => {
+ const verificacion = validateToken(token.split(' ')[1]);
+  if (verificacion.data){
+    return verificacion.data;
+  } else {
+    return null;
+  }
+}
 
 const server = new ApolloServer({
   typeDefs: tipos,
   resolvers: resolvers,
+  context: ({ req}) => {
+    const token = req.headers?.authorization ?? null;
+    if (token){
+      const userData = getUserData(token);
+      if (userData){
+        return { userData};
+      }
+    }
+    return null;
+  }
 });
 
 const app = express();
